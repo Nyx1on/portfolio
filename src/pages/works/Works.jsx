824fe45239a14ea7ac1559ec7ce115c93a44d912
Nirnay Behera <nirnay.1behera@gmail.com>
2023-05-12 15:20:43 +0530

    minor tweaks

--- src/pages/works/Works.jsx
+++ src/pages/works/Works.jsx
@@ -11,7 +11,7 @@ export default function Works({ menuOpen }) {
     max: 30, // max tilt rotation (degrees)
     perspective: 2000, // Transform perspective, the lower the more extreme the tilt gets.
     scale: 1, // 2 = 200%, 1.5 = 150%, etc..
-    speed: 5000, // Speed of the enter/exit transition
+    speed: 10000, // Speed of the enter/exit transition
     transition: true, // Set a transition on enter/exit.
     axis: null, // What axis should be disabled. Can be X or Y.
     reset: true, // If the tilt effect has to be reset on exit.
