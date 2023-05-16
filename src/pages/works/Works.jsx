fc48f20184c92dad2696c6220283e7bd281360cb
Nirnay Behera <nirnay.1behera@gmail.com>
2023-05-16 19:40:50 +0530

    update

--- src/pages/works/Works.jsx
+++ src/pages/works/Works.jsx
@@ -43,7 +43,133 @@ export default function Works({ menuOpen }) {
                       <div className="col-lg-4 col-sm-12">
                         <div className="project-wrapper__text load-hidden">
                           <h3 className="project-wrapper__text-title text-color-main">
-                            1. Pathfinding Vizualiser
+                            1. Sentiment Analysis
+                          </h3>
+                          <div>
+                            <p className="mb-4">
+                              Project Sean is a sentiment analysis project. It
+                              utilizes machine learning techniques, including
+                              TensorFlow and Keras, to analyze the sentiment of
+                              comments or reviews. By entering a comment into
+                              the prompt, users can gain valuable insights into
+                              the emotional tone of the text. Though still a
+                              work in progress, Project Sean aims to provide a
+                              user-friendly platform for sentiment analysis,
+                              leveraging the power of artificial intelligence
+                              and natural language processing.
+                            </p>
+                          </div>
+                          <a
+                            rel="noreferrer"
+                            target="_blank"
+                            className="cta-btn cta-btn--hero"
+                            href="#!"
+                          >
+                            See Live
+                          </a>
+                          <a
+                            rel="noreferrer"
+                            target="_blank"
+                            className="cta-btn text-color-main"
+                            href="https://github.com/Nyx1on/project-sean-fs"
+                          >
+                            Source Code
+                          </a>
+                        </div>
+                      </div>
+                      <Tilt
+                        options={defaultOptions}
+                        className="col-lg-8 col-sm-12"
+                      >
+                        <div className="project-wrapper__image load-hidden">
+                          <a
+                            rel="noreferrer"
+                            href="assets/sean.png"
+                            target="_blank"
+                          >
+                            <div className="thumbnail rounded">
+                              <img
+                                alt="Project"
+                                className="img-fluid"
+                                src="assets/sean.png"
+                              />
+                            </div>
+                          </a>
+                        </div>
+                      </Tilt>
+                    </div>
+                  </Fade>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <Fade>
+                    <div className="row">
+                      <div className="col-lg-4 col-sm-12">
+                        <div className="project-wrapper__text load-hidden">
+                          <h3 className="project-wrapper__text-title text-color-main">
+                            2. Creativai-2.0
+                          </h3>
+                          <div>
+                            <p className="mb-4">
+                              CreativAI 2.0 is a web application developed using
+                              the MERN stack, leveraging the OpenAI API to
+                              empower users in generating AI-generated
+                              photographs. This platform fosters a collaborative
+                              environment where users can showcase their
+                              creations. Users can easily find photographs by
+                              searching for keywords, tags, styles, or specific
+                              criteria, enhancing their ability to explore and
+                              access the diverse range of AI-generated photos
+                              within the platform.
+                            </p>
+                          </div>
+                          <a
+                            rel="noreferrer"
+                            target="_blank"
+                            className="cta-btn cta-btn--hero"
+                            href="#!"
+                          >
+                            See Live
+                          </a>
+                          <a
+                            rel="noreferrer"
+                            target="_blank"
+                            className="cta-btn text-color-main"
+                            href="https://github.com/Nyx1on/creativai-2.0"
+                          >
+                            Source Code
+                          </a>
+                        </div>
+                      </div>
+                      <Tilt
+                        options={defaultOptions}
+                        className="col-lg-8 col-sm-12"
+                      >
+                        <div className="project-wrapper__image load-hidden">
+                          <a
+                            rel="noreferrer"
+                            href="assets/sean.png"
+                            target="_blank"
+                          >
+                            <div className="thumbnail rounded">
+                              <img
+                                alt="Project"
+                                className="img-fluid"
+                                src="assets/creativeai-2.0-2.png"
+                              />
+                            </div>
+                          </a>
+                        </div>
+                      </Tilt>
+                    </div>
+                  </Fade>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <Fade>
+                    <div className="row">
+                      <div className="col-lg-4 col-sm-12">
+                        <div className="project-wrapper__text load-hidden">
+                          <h3 className="project-wrapper__text-title text-color-main">
+                            3. Pathfinding Vizualiser
                           </h3>
                           <div>
                             <p className="mb-4">
@@ -83,7 +209,11 @@ export default function Works({ menuOpen }) {
                         className="col-lg-8 col-sm-12"
                       >
                         <div className="project-wrapper__image load-hidden">
-                          <a rel="noreferrer" href="assets/pathfinding_v.png" target="_blank">
+                          <a
+                            rel="noreferrer"
+                            href="assets/pathfinding_v.png"
+                            target="_blank"
+                          >
                             <div className="thumbnail rounded">
                               <img
                                 alt="Project"
@@ -103,20 +233,20 @@ export default function Works({ menuOpen }) {
                       <div className="col-lg-4 col-sm-12">
                         <div className="project-wrapper__text load-hidden">
                           <h3 className="project-wrapper__text-title text-color-main">
-                            2. Task Management Software
+                            4. Task Management Software
                           </h3>
                           <div>
                             <p className="mb-4">
-                              Describe the project - Lorem ipsum dolor sit amet,
-                              consectetur adipiscing elit, sed do eiusmod tempor
-                              incididunt ut labore et dolore magna aliqua. Ut
-                              enim ad minim veniam, quis nostrud exercitation
-                              ullamco laboris nisi ut aliquip ex ea commodo
-                              consequat. Duis aute irure dolor in reprehenderit
-                              in voluptate velit esse cillum dolore eu fugiat
-                              nulla pariatur. Excepteur sint occaecat cupidatat
-                              non proident, sunt in culpa qui officia deserunt
-                              mollit anim id est laborum.
+                              Task Management Software (TMS) is a comprehensive
+                              solution built with React, Firebase, and Sass.
+                              With TMS, users can securely authenticate, create,
+                              and efficiently manage tasks. This intuitive
+                              software enables users to organize and prioritize
+                              tasks, set deadlines, assign team members, and
+                              track progress. Streamline your task management
+                              process with TMS, offering a seamless user
+                              experience and powerful features for enhanced
+                              productivity.
                             </p>
                           </div>
                           <a
@@ -142,7 +272,11 @@ export default function Works({ menuOpen }) {
                         className="col-lg-8 col-sm-12"
                       >
                         <div className="project-wrapper__image load-hidden">
-                          <a rel="noreferrer" href="assets/tms.png" target="_blank">
+                          <a
+                            rel="noreferrer"
+                            href="assets/tms.png"
+                            target="_blank"
+                          >
                             <div className="thumbnail rounded">
                               <img
                                 alt="Project"
@@ -162,20 +296,22 @@ export default function Works({ menuOpen }) {
                       <div className="col-lg-4 col-sm-12">
                         <div className="project-wrapper__text load-hidden">
                           <h3 className="project-wrapper__text-title text-color-main">
-                            3. Crowd Funding App
+                            5. Crowd Funding App
                           </h3>
                           <div>
                             <p className="mb-4">
-                              Describe the project - Lorem ipsum dolor sit amet,
-                              consectetur adipiscing elit, sed do eiusmod tempor
-                              incididunt ut labore et dolore magna aliqua. Ut
-                              enim ad minim veniam, quis nostrud exercitation
-                              ullamco laboris nisi ut aliquip ex ea commodo
-                              consequat. Duis aute irure dolor in reprehenderit
-                              in voluptate velit esse cillum dolore eu fugiat
-                              nulla pariatur. Excepteur sint occaecat cupidatat
-                              non proident, sunt in culpa qui officia deserunt
-                              mollit anim id est laborum.
+                              The crowdfunding application is built on Ethereum
+                              blockchain technology and smart contracts. This
+                              platform allows users to create and participate in
+                              fundraising campaigns using cryptocurrency (ETH).
+                              Through smart contracts, contributors can securely
+                              pledge funds, and project creators can
+                              transparently manage and distribute funds based on
+                              predefined conditions. CrowdFundETH ensures a
+                              decentralized and efficient crowdfunding
+                              experience, empowering individuals to support and
+                              launch projects while leveraging the benefits of
+                              blockchain and smart contract technology.
                             </p>
                           </div>
                           <a
@@ -201,12 +337,16 @@ export default function Works({ menuOpen }) {
                         className="col-lg-8 col-sm-12"
                       >
                         <div className="project-wrapper__image load-hidden">
-                          <a rel="noreferrer" href="assets/pathfinding_v3.png" target="_blank">
+                          <a
+                            rel="noreferrer"
+                            href="assets/pathfinding_v3.png"
+                            target="_blank"
+                          >
                             <div className="thumbnail rounded">
                               <img
                                 alt="Project"
                                 className="img-fluid"
-                                src="assets/pathfinding_v3.png"
+                                src="assets/no_preview.png"
                               />
                             </div>
                           </a>
@@ -220,7 +360,7 @@ export default function Works({ menuOpen }) {
           </div>
         </section>
       </Fade>
-      <DownArrow nextPage="contacts" curPage="my-works"/>
+      <DownArrow nextPage="contacts" curPage="my-works" />
     </div>
   );
 }
